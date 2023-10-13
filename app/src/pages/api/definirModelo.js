d148054d347142409a410de4fd3a9ce178293dff
TadeoPariani <tadeopariani1@gmail.com>
2023-10-13 18:53:21 -0300

    Se Genera el modelo

--- app/src/pages/api/definirModelo.js
+++ app/src/pages/api/definirModelo.js
@@ -1,9 +1,20 @@
 import { Sequelize, DataTypes, Model } from 'sequelize'
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+  });
 
 export function definirModelo(nombre, campos) {
 
     console.log("ESTO ES DEFINIR MODELO ", nombre, campos);
     const modelDefinition = {};
+
+    // try {
+    //     sequelize.authenticate();
+    //     console.log('Connection has been established successfully.');
+    //   } catch (error) {
+    //     console.error('Unable to connect to the database:', error);
+    // }
 
     campos.forEach(campo => {
         const { nombre: nombreCampo, tipo, esUnico } = campo;
@@ -17,6 +28,7 @@ export function definirModelo(nombre, campos) {
     console.log("ESTA ES LA DEFINICON DEL MODELO: ", modelDefinition)
 
     const Modelo = sequelize.define(nombre, modelDefinition);
+    Modelo.sync({ alter: true });
 
     //   return Modelo;
 }
