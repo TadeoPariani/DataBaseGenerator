4c3932c00feebc82e78fa7137f76ac0004f6ccee
TadeoPariani <tadeopariani1@gmail.com>
2023-10-26 21:33:11 -0300

    Refactorizacion de /modelos

--- app/src/pages/modelos/index.jsx
+++ app/src/pages/modelos/index.jsx
@@ -3,86 +3,49 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 function index() {
-
   const router = useRouter();
   const { lista } = router.query;
-  if (lista) {
-    const listaModelos2 = JSON.parse(decodeURIComponent(lista));
-  } 
+  let listaModelos = [];
 
-
+  try{
+    listaModelos = JSON.parse(decodeURIComponent(lista));
+  } catch (e){
+    console.log("error")
+  }
   
-  
-  // const [listaModelos, setListaModelos] = useState([]);
-
-  // useEffect(() => {
-  //     async function obtenerLista() {
-  //       try {
-  //         const response = await fetch('/api/definirModelos', {
-  //             method: 'GET',
-  //             headers: { 'Content-Type': 'application/json' },
-  //         });
-  //         const data = await response.json();
-  //         const listaModelos = data.data
-  //         setListaModelos(listaModelos)
-  //       } catch (error) {
-  //         console.error('Error al obtener la lista:', error);
-  //       }
-  //     }
-  //     obtenerLista();
-  // });
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    alert(JSON.stringify(listaModelos2))
+    alert(JSON.stringify(listaModelos))
     const response = await fetch('/api/definirModelos', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({listaModelos: listaModelos2})
+      body: JSON.stringify({listaModelos: listaModelos})
     });
-
-    // router.push({
-    //   pathname: '/modelos',
-    // })
 
     if (response.ok) {
       alert("Se creo Correctamente");
       await response.json();
-      router.push(`/modelos?lista=${JSON.stringify(listaModelos2)}`)
+      router.push(`/modelos?lista=${JSON.stringify(listaModelos)}`)
     } else {
       alert('Error al crear el modelo');
     }
   };
 
-
-
   return(
     <div>
-      <>
-        <h1>Tablas Creadas</h1>
-        <button type="submit" onClick={handleSubmit}>Crear Tablas</button>
-        {console.log(listaModelos2)}
-        <Tabla listaModelos2={listaModelos2}></Tabla>
-      </>
+      {listaModelos.length === 0 ? (
+        <div>NO HAY TABLAS CREADAS</div>
+      ) : (
+        <>
+          <h1>Tablas Creadas</h1>
+          <button type="submit" onClick={handleSubmit}>Crear Tablas</button>
+          {console.log(listaModelos)}
+          <Tabla listaModelos2={listaModelos}></Tabla>
+        </>
+      )}
     </div>
-
-
-
-
-    // <div>
-    //   {listaModelos2.length === 0 ? (
-    //     <div>NO HAY TABLAS CREADAS</div>
-    //   ) : (
-    //     <>
-    //       <h1>Tablas Creadas</h1>
-    //       <button type="submit" onClick={handleSubmit}>Crear Tablas</button>
-    //       {console.log(listaModelos2)}
-    //       <Tabla listaModelos2={listaModelos2}></Tabla>
-    //     </>
-    //   )}
-    // </div>
   )
 }
 
