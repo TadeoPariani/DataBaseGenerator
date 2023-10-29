4beac330b613e8e30861d3da6f98e145085826b7
joaco peralta <joacoperalta99@outlook.com>
2023-10-29 03:28:39 -0600

    Componente footer añadido

---
+++ app/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-teal-950 text-white p-4 mt-auto">
+      <div className="mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+            <img src="logoDB.png" alt="Logo" className="h-12 w-12 mr-4" />
+          <span className="text-lg font-bold text-orange-300">DatabaseGenerator</span>
+        </div>
+        <div className="flex space-x-4">
+          <img src="github.png" alt="Logo" className="h-8 w-9 mr-4" />
+          <a href="https://github.com/TadeoPariani" className="hover:text-indigo-500">Contacto</a>
+          <img src="contact.png" alt="Logo" className="h-8 w-9" />
+          <a href="/https://github.com/TadeoPariani/DataBaseGenerator" className="hover:text-indigo-500">Info</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
